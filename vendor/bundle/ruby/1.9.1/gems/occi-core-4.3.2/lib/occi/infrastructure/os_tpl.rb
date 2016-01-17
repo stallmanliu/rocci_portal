cd38203ee4c991901f8cd6f155bbaab30a12b7d6
stallmanliu <daniel@daniels-MacMini.local>
2016-01-16 21:58:51 -0500

    regular git save to master

--- vendor/bundle/ruby/1.9.1/gems/occi-core-4.3.2/lib/occi/infrastructure/os_tpl.rb
+++ vendor/bundle/ruby/1.9.1/gems/occi-core-4.3.2/lib/occi/infrastructure/os_tpl.rb
@@ -1,5 +1,38 @@
 module Occi
   module Infrastructure
+    
+    class OsTpl < Occi::Core::Mixin
+      
+      mattr_accessor :attributes, :mixin
+      #http://schemas.ogf.org/occi/infrastructure/os_tpl/action
+      clone = Occi::Core::Action.new scheme='http://schemas.ogf.org/occi/infrastructure/os_tpl/action#',
+                                      term='clone',
+                                      title='clone os_tpl'
+                                      
+      self.actions = Occi::Core::Actions.new << clone
+      
+      self.attributes = Occi::Core::Attributes.new(Occi::Core::Mixin.attributes)
+
+      self.mixin = Occi::Core::Mixin.new scheme='http://schemas.ogf.org/occi/infrastructure#',
+                                         term='os_tpl',
+                                         title='operating system template',
+                                         attributes=Occi::Core::Attributes.new(self.attributes),
+                                         dependencies=Occi::Core::Dependencies.new,
+                                         actions=Occi::Core::Actions.new,
+                                         location='/mixin/os_tpl/',
+                                         applies=Occi::Core::Kinds.new << Occi::Infrastructure::Compute.kind
+
+      self.kind = Occi::Core::Kind.new scheme='http://schemas.ogf.org/occi/infrastructure#',
+                                       term='os_tpl',
+                                       title = 'operating system template',
+                                       attributes = Occi::Core::Attributes.new(self.attributes),
+                                       parent=Occi::Core::Mixin.kind,
+                                       actions = Occi::Core::Actions.new(self.actions),
+                                       location = '/mixin/os_tpl/'
+      
+    end
+    
+=begin    
     module OsTpl
 
       mattr_accessor :attributes, :mixin
@@ -15,6 +48,7 @@ module Occi
                                          location='/mixin/os_tpl/',
                                          applies=Occi::Core::Kinds.new << Occi::Infrastructure::Compute.kind
     end
-
+=end
+    
   end
 end
